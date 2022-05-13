7ffaf4b034ce886b61244ad641f1eaeca79929cc
Denner Santos <denners820@gmail.com>
2022-05-13 14:55:27 -0300

    Escape

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+var nome1="Denner"
+console.log(nome1)
+
+let nome2="Neuza"
+console.log(nome2)
+
+const nome3="Manoel"
+console.log(nome3)
+
+nome1="Denner Santos"
+console.log(nome1)
+
+nome2="Neuza Oliveira"
+console.log(nome2)
